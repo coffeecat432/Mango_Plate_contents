7079ec54825b9e2d339c5313b3ce6243741fbae6
coffeecat432 <coffeecat432@gmail.com>
2024-08-07 20:03:17 +0900

    개별 item 표시, RecyclerView 만들기

--- app/src/main/java/com/amigo/mango_contents/MainActivity.kt
+++ app/src/main/java/com/amigo/mango_contents/MainActivity.kt
@@ -5,16 +5,164 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val items = mutableListOf<ContentsModel>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/350600",
+                "https://img.siksinhot.com/place/1460622895364757.png?w=540&h=436&c=Y",
+                "밍글스",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/320351",
+                "https://img.siksinhot.com/place/1554870952814204.jpg?w=540&h=436&c=Y",
+                "삼성원조양곱창",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/320334",
+                "https://img.siksinhot.com/place/1530594902236001.jpg?w=540&h=436&c=Y",
+                "뜨락",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/363138",
+                "https://img.siksinhot.com/find/1459329005595123.jpg?w=540&h=436&c=X",
+                "권숙수",
+            )
+        )
+
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/350600",
+                "https://img.siksinhot.com/place/1460622895364757.png?w=540&h=436&c=Y",
+                "밍글스",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/320351",
+                "https://img.siksinhot.com/place/1554870952814204.jpg?w=540&h=436&c=Y",
+                "삼성원조양곱창",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/320334",
+                "https://img.siksinhot.com/place/1530594902236001.jpg?w=540&h=436&c=Y",
+                "뜨락",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/363138",
+                "https://img.siksinhot.com/find/1459329005595123.jpg?w=540&h=436&c=X",
+                "권숙수",
+            )
+        )
+
+
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/350600",
+                "https://img.siksinhot.com/place/1460622895364757.png?w=540&h=436&c=Y",
+                "밍글스",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/320351",
+                "https://img.siksinhot.com/place/1554870952814204.jpg?w=540&h=436&c=Y",
+                "삼성원조양곱창",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/320334",
+                "https://img.siksinhot.com/place/1530594902236001.jpg?w=540&h=436&c=Y",
+                "뜨락",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/363138",
+                "https://img.siksinhot.com/find/1459329005595123.jpg?w=540&h=436&c=X",
+                "권숙수",
+            )
+        )
+
+
+
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/350600",
+                "https://img.siksinhot.com/place/1460622895364757.png?w=540&h=436&c=Y",
+                "밍글스",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/320351",
+                "https://img.siksinhot.com/place/1554870952814204.jpg?w=540&h=436&c=Y",
+                "삼성원조양곱창",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/320334",
+                "https://img.siksinhot.com/place/1530594902236001.jpg?w=540&h=436&c=Y",
+                "뜨락",
+            )
+        )
+
+        items.add(
+            ContentsModel(
+                "https://www.siksinhot.com/P/363138",
+                "https://img.siksinhot.com/find/1459329005595123.jpg?w=540&h=436&c=X",
+                "권숙수",
+            )
+        )
+
+
+
+
+        val recyclerview = findViewById<RecyclerView>(R.id.rv)
+        val rvAdapter = RVAdapter(baseContext, items)
+        recyclerview.adapter = rvAdapter
+        //recyclerview.layoutManager = LinearLayoutManager(this)
+        recyclerview.layoutManager = GridLayoutManager(this, 2)
+
+
+
     }
 }
